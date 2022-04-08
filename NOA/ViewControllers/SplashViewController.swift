3c0864e6167d6c811152ecf6a10855289ece7935
wi-seong-cheol <tjd9248@naver.com>
2022-04-08 23:24:37 +0900

    SplashViewController Create

---
+++ NOA/ViewControllers/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/08.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
