0c4e48dd5d54b96905331f8b4f4a6118af1102dd
wi-seong-cheol <tjd9248@naver.com>
2022-04-09 23:53:01 +0900

    HomeViewModel Add

---
+++ NOA/ViewModels/HomeViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  HomeViewModel.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/09.
+//
+
+import Foundation
+
+class HomeViewModel: NSObject {
+//    @IBOutlet var service: NoaRepository!
+
+    private var feedList: FeedResult = FeedResult.EMPTY
+    private var loading = false
+    
+    var loadingStarted: () -> Void = { }
+    var loadingEnded: () -> Void = { }
+    var lectureListUpdated: () -> Void = { }
+    
+    func feedsCount() -> Int {
+        return feedList.list.count
+    }
+
+    func lecture(at index: Int) -> Feed {
+        return feedList.list[index]
+    }
+
+    func list() {
+//        loading = true
+//        loadingStarted()
+//        service.list {
+//            self.lectureList = $0
+//            self.lectureListUpdated()
+//            self.loadingEnded()
+//            self.loading = false
+//        }
+    }
+
+    func next() {
+//        if loading { return }
+//        loading = true
+//        loadingStarted()
+//        service.next(currentPage: lectureList) {
+//            var lectureList = $0
+//            lectureList.lectures.insert(contentsOf: self.lectureList.lectures, at: 0)
+//            self.lectureList = lectureList
+//            self.lectureListUpdated()
+//            self.loadingEnded()
+//            self.loading = false
+//        }
+    }
+}
