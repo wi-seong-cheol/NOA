97af5a1ec1f837f7b61352a35263e6b4818d61f7
wi-seong-cheol <tjd9248@naver.com>
2022-04-09 23:53:34 +0900

    TableView Setting

---
+++ NOA/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/03/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var viewModel: HomeViewModel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    func configure() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        print(viewModel.feedsCount())
+        return viewModel.feedsCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedCell.identifier, for: indexPath) as? FeedCell else {
+            return UITableViewCell()
+        }
+        
+//        let feed = viewModel (at: indexPath.row)
+//        cell.configure(with: feed)
+
+        return cell
+    }
+}
