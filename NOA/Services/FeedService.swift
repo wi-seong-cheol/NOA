64b0e50eaddcd02d261613697f2ad3273aa6da8e
wi-seong-cheol <tjd9248@naver.com>
2022-04-13 17:29:46 +0900

    FeedService Add

--- NOA/Services/FeedService.swift
+++ NOA/Services/FeedService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import RxSwift
+
+class PostService {
+    
+    @Inject private var apiUrlService: APIUrlService
+    @Inject private var apiRequestService: APIRequestService
+    
+    //포스트 리스트 조회 메소드
+    func getPostList()-> Observable<[FeedResponse]>? {
+        let URL = apiUrlService.serviceUrl(version: "", path: "/posts")
+        
+        return apiRequestService.getable(URL: URL, query: nil, interceptor: .none)
+    }
+}
