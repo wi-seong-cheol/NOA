7959d562843896c31b051e3263a5a99fb1a0294f
wi-seong-cheol <tjd9248@naver.com>
2022-04-13 17:26:56 +0900

    BaseInterceptor Add

---
+++ NOA/Services/BaseInterceptor.swift
@@ -0,0 +1,50 @@
+//
+//  BaseInterceptor.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/13.
+//
+
+import Alamofire
+
+final class BaseInterceptor: RequestInterceptor {
+//    private let retryLimit = 1
+//    private var retryCount = 0
+//    
+//    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+//        print("BaseInterceptor  - adpat() called")
+//        
+//        var request = urlRequest
+//        let tk = Token()
+//        
+//        guard let accessToken = tk.load(account: "accessToken") else {
+//            return
+//        }
+//        
+//        request.headers.add(.authorization(bearerToken: accessToken))
+//        completion(.success(request))
+//    }
+//    
+//    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+//        
+//        guard let statusCode = request.response?.statusCode else {
+//            completion(.doNotRetry)
+//            return
+//        }
+//        
+//        switch statusCode {
+//        case 401:
+//            if retryCount < retryLimit {
+//                retryCount += 1
+//                print("BaseInterceptor  - retry() called")
+//                let status = Methods.shared.refresh()
+//                print(status)
+//                completion(.retryWithDelay(1))
+//            } else {
+//                completion(.doNotRetry)
+//            }
+//        default:
+//            completion(.doNotRetry)
+//        }
+//    }
+}
