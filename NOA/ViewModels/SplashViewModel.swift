b2fec3766f0d077682ba17ce6b61ccf5deb36408
wi-seong-cheol <tjd9248@naver.com>
2022-04-14 16:57:19 +0900

    SplashViewModel Update

--- NOA/ViewModels/SplashViewModel.swift
+++ NOA/ViewModels/SplashViewModel.swift
@@ -6,27 +6,95 @@
 //
 
 import Foundation
+import RxSwift
 
-class SplashViewModel: NSObject {
+protocol SplashViewModelType {
+    // MARK: INPUT
     
-    var loadingStarted: () -> Void = { }
-    var loadingEnded: () -> Void = { }
+    var fetchList: AnyObserver<Void> { get }
     
-    func initialization() {
-        loadingStarted()
-        // API Call
-        loadingEnded()
-    }
+    // MARK: OUTPUT
     
-    func versionCheck() {
+    var lectureList: Observable<LectureList> { get }
+    var errorMessage: Observable<NSError> { get }
+}
+
+class SplashViewModel: SplashViewModelType {
+    
+    let disposeBag = DisposeBag()
+    
+    // MARK: OUTPUT
+//    let user: Observable<[UserResponse]>
+//
+//    init(service: UserFetchable = UserService()) {
+//
+//            }, onError: <#T##((Error) -> Void)?##((Error) -> Void)?##(Error) -> Void#>, onCompleted: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDisposed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+//        let loadingStarted = Observable<Void>.empty()
+//        let loadingEnded = Observable<Void>.empty()
+//
+//        loadingStarted
+//            .do(onNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, afterNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, onError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, afterError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, onCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, afterCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, onSubscribe: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onSubscribed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDispose: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+//
+//                loadingEnded.do(onNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, afterNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, onError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, afterError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, onCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, afterCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, onSubscribe: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onSubscribed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDispose: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+//    }
+    // MARK: INPUT
+    
+    let fetchList: AnyObserver<Void>
+    
+    // MARK: OUTPUT
+    
+    let lectureList: Observable<LectureList>
+    let errorMessage: Observable<NSError>
+    
+    init(service: UserFetchable = UserService()) {
+        let fetching = PublishSubject<Void>()
         
-    }
-    
-    func concurrentWorkItems() {
+        let list = BehaviorSubject<LectureList>(value: LectureList.EMPTY)
+        let activating = BehaviorSubject<Bool>(value: false)
+        let error = PublishSubject<Error>()
         
-    }
-    
-    func comparePushToken() {
+        // MARK: INPUT
+        fetchList = fetching.asObserver()
         
+        fetching
+            .do(onNext: { _ in activating.onNext(true) })
+            .flatMap(service.getTestData)
+            .do(onNext: { _ in activating.onNext(false) })
+            .do(onError: { err in error.onNext(err) })
+            .subscribe(onNext: list.onNext)
+            .disposed(by: disposeBag)
+//            .do(onNext: { _ in activating.onNext(true) })
+//                .flatMap(service.getTestData)
+//                .map { $0.map { LectureList(from: $0) } }
+//                .do(onNext: { _ in activating.onNext(false) })
+//                .do(onError: { err in error.onNext(err) })
+//                .subscribe(onNext: lectureList.onNext)
+//                .disposed(by: disposeBag)
+                
+        // MARK: OUTPUT
+        lectureList = list
+
+        errorMessage = error.map { $0 as NSError }
     }
+//
+//    var loadingStarted: () -> Void = { }
+//    var loadingEnded: () -> Void = { }
+//
+//    func initialization() {
+//        loadingStarted()
+//        // API Call
+//        loadingEnded()
+//    }
+//
+//    func versionCheck() {
+//
+//    }
+//
+//    func concurrentWorkItems() {
+//
+//    }
+//
+//    func comparePushToken() {
+//
+//    }
 }
