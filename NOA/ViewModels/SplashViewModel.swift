d68d49e1bdf509b484b46ef67a69cfa5c2c23966
wi-seong-cheol <tjd9248@naver.com>
2022-04-14 17:21:10 +0900

    SplashViewModel Update

--- NOA/ViewModels/SplashViewModel.swift
+++ NOA/ViewModels/SplashViewModel.swift
@@ -16,6 +16,7 @@ protocol SplashViewModelType {
     // MARK: OUTPUT
     
     var lectureList: Observable<LectureList> { get }
+    var activated: Observable<Bool> { get }
     var errorMessage: Observable<NSError> { get }
 }
 
@@ -23,20 +24,6 @@ class SplashViewModel: SplashViewModelType {
     
     let disposeBag = DisposeBag()
     
-    // MARK: OUTPUT
-//    let user: Observable<[UserResponse]>
-//
-//    init(service: UserFetchable = UserService()) {
-//
-//            }, onError: <#T##((Error) -> Void)?##((Error) -> Void)?##(Error) -> Void#>, onCompleted: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDisposed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-//        let loadingStarted = Observable<Void>.empty()
-//        let loadingEnded = Observable<Void>.empty()
-//
-//        loadingStarted
-//            .do(onNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, afterNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, onError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, afterError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, onCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, afterCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, onSubscribe: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onSubscribed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDispose: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-//
-//                loadingEnded.do(onNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, afterNext: <#T##((()) throws -> Void)?##((()) throws -> Void)?##(()) throws -> Void#>, onError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, afterError: <#T##((Error) throws -> Void)?##((Error) throws -> Void)?##(Error) throws -> Void#>, onCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, afterCompleted: <#T##(() throws -> Void)?##(() throws -> Void)?##() throws -> Void#>, onSubscribe: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onSubscribed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDispose: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-//    }
     // MARK: INPUT
     
     let fetchList: AnyObserver<Void>
@@ -44,6 +31,7 @@ class SplashViewModel: SplashViewModelType {
     // MARK: OUTPUT
     
     let lectureList: Observable<LectureList>
+    let activated: Observable<Bool>
     let errorMessage: Observable<NSError>
     
     init(service: UserFetchable = UserService()) {
@@ -63,38 +51,12 @@ class SplashViewModel: SplashViewModelType {
             .do(onError: { err in error.onNext(err) })
             .subscribe(onNext: list.onNext)
             .disposed(by: disposeBag)
-//            .do(onNext: { _ in activating.onNext(true) })
-//                .flatMap(service.getTestData)
-//                .map { $0.map { LectureList(from: $0) } }
-//                .do(onNext: { _ in activating.onNext(false) })
-//                .do(onError: { err in error.onNext(err) })
-//                .subscribe(onNext: lectureList.onNext)
-//                .disposed(by: disposeBag)
                 
         // MARK: OUTPUT
         lectureList = list
 
         errorMessage = error.map { $0 as NSError }
+        
+        activated = activating.distinctUntilChanged()
     }
-//
-//    var loadingStarted: () -> Void = { }
-//    var loadingEnded: () -> Void = { }
-//
-//    func initialization() {
-//        loadingStarted()
-//        // API Call
-//        loadingEnded()
-//    }
-//
-//    func versionCheck() {
-//
-//    }
-//
-//    func concurrentWorkItems() {
-//
-//    }
-//
-//    func comparePushToken() {
-//
-//    }
 }
