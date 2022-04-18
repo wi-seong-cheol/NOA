2de971d60a835670fed6b075dd44e910630f4a52
wi-seong-cheol <tjd9248@naver.com>
2022-04-18 23:28:28 +0900

    FeedService Update

--- NOA/Services/FeedService.swift
+++ NOA/Services/FeedService.swift
@@ -9,8 +9,7 @@ import Foundation
 import RxSwift
 
 protocol FeedFetchable {
-    func getFeedList()-> Observable<LectureList>
-    func getFeedNextList(currentPage: LectureList) -> Observable<LectureList>
+    func getFeedList(currentPage: Int) -> Observable<LectureList>
 }
 
 class FeedService: FeedFetchable {
@@ -19,16 +18,9 @@ class FeedService: FeedFetchable {
     @Inject private var apiRequestService: APIRequestService
     
     //포스트 리스트 조회 메소드
-    func getFeedList()-> Observable<LectureList> {
+    func getFeedList(currentPage: Int) -> Observable<LectureList> {
         let URL = "http://apis.data.go.kr/B552881/kmooc/courseList?serviceKey=LwG%2BoHC0C5JRfLyvNtKkR94KYuT2QYNXOT5ONKk65iVxzMXLHF7SMWcuDqKMnT%2BfSMP61nqqh6Nj7cloXRQXLA%3D%3D"
-        let query: [String: Any] = ["Mobile": 1]
-        //        let URL = apiUrlService.serviceUrl(version: "", path: "/courseList")
-        //        let query: [String: Any] = ["serviceKey": DBInfo.serviceKey, "Mobile": 1]
+        let query: [String: Any] = currentPage == 1 ? ["Mobile": 1] : ["Mobile": 1, "page": currentPage]
         return apiRequestService.getable(URL: URL, query: query, interceptor: .none) ?? Observable.empty()
-    }
-    
-    func getFeedNextList(currentPage: LectureList) -> Observable<LectureList> {
-        let URL = currentPage.next
-        return apiRequestService.getable(URL: URL, query: [:], interceptor: .none) ?? Observable.empty()
     }
 }
