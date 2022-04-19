d8d0e558c52df4147d14774d2ddf50c24ba1ef13
wi-seong-cheol <tjd9248@naver.com>
2022-04-19 15:39:40 +0900

    Feed Service Pagination

--- NOA/Services/FeedService.swift
+++ NOA/Services/FeedService.swift
@@ -9,7 +9,8 @@ import Foundation
 import RxSwift
 
 protocol FeedFetchable {
-    func getFeedList(currentPage: Int) -> Observable<LectureList>
+    func list() -> Observable<LectureList>
+    func next(nextPage: String) -> Observable<LectureList>
 }
 
 class FeedService: FeedFetchable {
@@ -18,9 +19,14 @@ class FeedService: FeedFetchable {
     @Inject private var apiRequestService: APIRequestService
     
     //포스트 리스트 조회 메소드
-    func getFeedList(currentPage: Int) -> Observable<LectureList> {
+    func list() -> Observable<LectureList> {
         let URL = "http://apis.data.go.kr/B552881/kmooc/courseList?serviceKey=LwG%2BoHC0C5JRfLyvNtKkR94KYuT2QYNXOT5ONKk65iVxzMXLHF7SMWcuDqKMnT%2BfSMP61nqqh6Nj7cloXRQXLA%3D%3D"
-        let query: [String: Any] = currentPage == 1 ? ["Mobile": 1] : ["Mobile": 1, "page": currentPage]
+        let query: [String: Any] = ["Mobile": 1]
         return apiRequestService.getable(URL: URL, query: query, interceptor: .none) ?? Observable.empty()
+    }
+    
+    func next(nextPage: String) -> Observable<LectureList> {
+        let URL = nextPage
+        return apiRequestService.getable(URL: URL, query: [:], interceptor: .none) ?? Observable.empty()
     }
 }
