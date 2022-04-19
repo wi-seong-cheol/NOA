4437019972e17c920b570a16148c40c329047c58
wi-seong-cheol <tjd9248@naver.com>
2022-04-19 15:40:14 +0900

    HomeViewModel Pagination

--- NOA/ViewModels/HomeViewModel.swift
+++ NOA/ViewModels/HomeViewModel.swift
@@ -12,10 +12,11 @@ import RxRelay
 protocol HomeViewModelType {
     // MARK: INPUT
     var fetchList: AnyObserver<Void> { get }
+    var moreFetchList: AnyObserver<Void> { get }
     var likeTap: AnyObserver<Void> { get }
     
     // MARK: OUTPUT
-    var lectureList: Observable<LectureList> { get }
+    var items: Observable<[Lecture]> { get }
     var activated: Observable<Bool> { get }
     var errorMessage: Observable<NSError> { get }
 }
@@ -25,33 +26,51 @@ class HomeViewModel: HomeViewModelType {
     let disposeBag = DisposeBag()
     
     // MARK: INPUT
-    
     let fetchList: AnyObserver<Void>
+    let moreFetchList: AnyObserver<Void>
     let likeTap: AnyObserver<Void>
     
     // MARK: OUTPUT
-    
-    let lectureList: Observable<LectureList>
+    let items: Observable<[Lecture]>
     let activated: Observable<Bool>
     let errorMessage: Observable<NSError>
     
     init(service: FeedFetchable = FeedService()) {
         let fetching = PublishSubject<Void>()
+        let moreFetching = PublishSubject<Void>()
         let liking = PublishSubject<Void>()
         
-        let list = BehaviorSubject<LectureList>(value: LectureList.EMPTY)
+        let itemsList = BehaviorRelay<[Lecture]>(value: [])
+        let nextPage = BehaviorRelay<String>(value: "")
+        
         let activating = BehaviorSubject<Bool>(value: false)
         let error = PublishSubject<Error>()
         
         // MARK: INPUT
         fetchList = fetching.asObserver()
+        moreFetchList = moreFetching.asObserver()
         
         fetching
             .do(onNext: { _ in activating.onNext(true) })
-            .flatMap(service.getFeedList)
+            .flatMap(service.list)
             .do(onNext: { _ in activating.onNext(false) })
             .do(onError: { err in error.onNext(err) })
-            .subscribe(onNext: list.onNext)
+            .subscribe(onNext: { response in
+                itemsList.accept(response.lectures)
+                nextPage.accept(response.next)
+            })
+            .disposed(by: disposeBag)
+                
+        moreFetching
+            .do(onNext: { _ in activating.onNext(true) })
+            .flatMapLatest{ service.next(nextPage: nextPage.value) }
+            .share(replay: 1)
+            .do(onNext: { _ in activating.onNext(false) })
+            .do(onError: { err in error.onNext(err) })
+            .subscribe(onNext: { response in
+                itemsList.accept(itemsList.value + response.lectures)
+                nextPage.accept(response.next)
+            })
             .disposed(by: disposeBag)
                 
         likeTap = liking.asObserver()
@@ -62,7 +81,7 @@ class HomeViewModel: HomeViewModelType {
 //                .disposed(by: disposeBag)
                 
         // MARK: OUTPUT
-        lectureList = list
+        items = itemsList.asObservable()
         
         errorMessage = error.map { $0 as NSError }
         
