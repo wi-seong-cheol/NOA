fe85419b2a93c0238b07cb05022d9710e5bb9d40
wi-seong-cheol <tjd9248@naver.com>
2022-04-20 15:19:42 +0900

    Add

---
+++ NOA/Modules/Feed/FeedCollectionCell.swift
@@ -0,0 +1,43 @@
+//
+//  FeedCollectionCell.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/20.
+//
+
+import UIKit
+import RxSwift
+
+class FeedCollectionCell: UICollectionViewCell {
+    static let identifier = "FeedCollectionCell"
+   
+    @IBOutlet var thumbnail: UIImageView!
+    
+    private let cellDisposeBag = DisposeBag()
+    
+    var disposeBag = DisposeBag()
+    let onData: AnyObserver<Lecture>
+    
+    required init?(coder aDecoder: NSCoder) {
+        let data = PublishSubject<Lecture>()
+
+        onData = data.asObserver()
+
+        super.init(coder: aDecoder)
+
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] lecture in
+                ImageLoader.loadImage(from: lecture.courseImage)
+                    .observe(on: MainScheduler.instance)
+                    .subscribe(onNext: { (image) in
+                        self?.thumbnail.image = image})
+                    .disposed(by: self!.disposeBag)
+            })
+            .disposed(by: cellDisposeBag)
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+}
