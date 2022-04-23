48a2e441795a99a80248ef2f0dbc49aa456ebcd4
wi-seong-cheol <tjd9248@naver.com>
2022-04-23 23:40:08 +0900

    ChatViewModel Add

---
+++ NOA/Modules/Chat/ChatViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewModel.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/23.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+protocol ChatViewModelType {
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+}
+
+class ChatViewModel: ChatViewModelType {
+    
+    let disposeBag = DisposeBag()
+    
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+    
+    init(service: FeedFetchable = FeedService()) {
+        
+    }
+}
+
