e32152b42fd7e56d9c92c2fdb167a6cadb797e58
wi-seong-cheol <tjd9248@naver.com>
2022-04-23 23:39:52 +0900

    ChatViewController Add

---
+++ NOA/Modules/Chat/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/23.
+//
+
+import RxCocoa
+import RxSwift
+import RxViewController
+import NVActivityIndicatorView
+import UIKit
+
+class ChatViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
