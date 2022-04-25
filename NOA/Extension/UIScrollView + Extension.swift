5cdf8c4e23b059fcc6fd49fb3ca792bdafd696de
wi-seong-cheol <tjd9248@naver.com>
2022-04-26 00:02:00 +0900

    Update

--- NOA/Extension/UIScrollView + Extension.swift
+++ NOA/Extension/UIScrollView + Extension.swift
@@ -5,4 +5,21 @@
 //  Created by wi_seong on 2022/04/19.
 //
 
-import Foundation
+import RxSwift
+
+extension UIScrollView {
+    var rx_reachedBottom: Observable<Void> {
+        return rx.contentOffset
+            .flatMap { [weak self] contentOffset -> Observable<Void> in
+                guard let scrollView = self else {
+                    return Observable.empty()
+                }
+                
+                let visibleHeight = scrollView.frame.height - scrollView.contentInset.top - scrollView.contentInset.bottom
+                let y = contentOffset.y + scrollView.contentInset.top
+                let threshold = max(0.0, scrollView.contentSize.height - visibleHeight)
+                
+                return y > threshold ? Observable.just(()) : Observable.empty()
+        }
+    }
+}
