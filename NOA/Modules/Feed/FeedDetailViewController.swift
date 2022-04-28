f6fdca6db1e2cccd02397fa5ad22666b0a677e7a
wi-seong-cheol <tjd9248@naver.com>
2022-04-28 22:57:28 +0900

    View Edit

--- NOA/Modules/Feed/FeedDetailViewController.swift
+++ NOA/Modules/Feed/FeedDetailViewController.swift
@@ -14,7 +14,15 @@ import UIKit
 class FeedDetailViewController: UIViewController {
     
     static let identifier = "FeedDetailViewController"
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var more: UIButton!
     @IBOutlet weak var work: UIImageView!
+    @IBOutlet weak var like: UIButton!
+    @IBOutlet weak var likeCount: UILabel!
+    @IBOutlet weak var nft: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var desc: UILabel!
     
     lazy var indicator: NVActivityIndicatorView = {
         let indicator = NVActivityIndicatorView(
@@ -54,16 +62,55 @@ class FeedDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configure()
         setupBindings()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
 
 extension FeedDetailViewController {
+    // MARK: - UI Setting
+    func configure() {
+        profile.layer.cornerRadius = profile.frame.width / 2
+        nickname.font = UIFont.NotoSansCJKkr(type: .medium, size: 14)
+        likeCount.font = UIFont.NotoSansCJKkr(type: .medium, size: 14)
+        titleLabel.font = UIFont.NotoSansCJKkr(type: .medium, size: 14)
+        desc.font = UIFont.NotoSansCJKkr(type: .regular, size: 14)
+    }
+    
     // MARK: - UI Binding
     func setupBindings() {
+        // ------------------------------
+        //     INPUT
+        // ------------------------------
         viewModel.work
             .bind(to: work.rx.image)
             .disposed(by: disposeBag)
         
+        viewModel.profile
+            .bind(to: profile.rx.image)
+            .disposed(by: disposeBag)
+        
+        // ------------------------------
+        //     Page Move
+        // ------------------------------
+
+        // 페이지 이동
+        
+        // ------------------------------
+        //     OUTPUT
+        // ------------------------------
+
+        // 에러 처리
+        viewModel.errorMessage
+            .map { $0.domain }
+            .subscribe(onNext: { [weak self] message in
+                self?.OKDialog("Order Fail")
+            }).disposed(by: disposeBag)
     }
 }
