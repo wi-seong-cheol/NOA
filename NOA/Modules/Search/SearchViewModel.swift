1e398bf96afff63b27237f131698b0be146469a2
wi-seong-cheol <tjd9248@naver.com>
2022-04-30 23:09:59 +0900

    SearchViewModel Add

---
+++ NOA/Modules/Search/SearchViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SearchViewModel.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/04/29.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+import RealmSwift
+
+protocol SearchViewModelType {
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+    var errorMessage: Observable<NSError> { get }
+}
+
+class SearchViewModel: SearchViewModelType {
+    
+    let disposeBag = DisposeBag()
+    
+    let realm = try! Realm()
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+    let errorMessage: Observable<NSError>
+    
+    init(service: FeedFetchable = FeedService()) {
+        let error = PublishSubject<Error>()
+        
+        // MARK: INPUT
+        
+        // MARK: OUTPUT
+        errorMessage = error.map { $0 as NSError }
+    }
+}
+
