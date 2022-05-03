24d843b2854a8d117b060fc30f45ee116364d370
wi-seong-cheol <tjd9248@naver.com>
2022-05-03 12:18:05 +0900

    Search Update

--- NOA/Services/SearchService.swift
+++ NOA/Services/SearchService.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol SearchFetchable {
+    func search(_ keyword: String) -> Observable<[Search]>
+}
+
+class SearchService: SearchFetchable {
+    var cache = [String: [Search]]()
+    
+    @Inject private var apiUrlService: APIUrlService
+    @Inject private var apiRequestService: APIRequestService
+    
+    //포스트 리스트 조회 메소드
+    func search(_ keyword: String) -> Observable<[Search]> {
+        guard !keyword.isEmpty else {
+            return Observable.just([])
+        }
+        let URL = "https://api.github.com/users/\(keyword)/repos"
+        if keyword != "", let cachedData = self.cache[keyword] {
+            return Observable.just(cachedData)
+        } else {
+            return apiRequestService.getable(URL: URL, query: [:], interceptor: .none)!
+                .do(onNext: { data in
+                    if keyword != "" {
+                        self.cache[keyword] = data
+                    }
+                })
+                .catch{ error in
+                    if keyword != "", let cachedData = self.cache[keyword] {
+                        return Observable.just(cachedData)
+                    } else {
+                        return Observable.just([])
+                    }
+                    
+                }
+        }
+    }
+}
+
