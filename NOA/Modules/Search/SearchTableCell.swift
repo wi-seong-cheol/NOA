421c8905f3cf11e2128888eed70fe58a285390f7
wi-seong-cheol <tjd9248@naver.com>
2022-05-03 15:25:02 +0900

    Refactoring

--- NOA/Modules/Search/SearchTableCell.swift
+++ NOA/Modules/Search/SearchTableCell.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+
+class SearchTableCell: UITableViewCell {
+    static let identifier = "SearchTableCell"
+    
+    private let cellDisposeBag = DisposeBag()
+    @IBOutlet weak var word: UILabel!
+    
+    var disposeBag = DisposeBag()
+    let onData: AnyObserver<Search>
+    
+    required init?(coder aDecoder: NSCoder) {
+        let data = PublishSubject<Search>()
+
+        onData = data.asObserver()
+        
+        super.init(coder: aDecoder)
+        
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] item in
+                self?.word.text = item.repoName
+            })
+            .disposed(by: cellDisposeBag)
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+}
+
