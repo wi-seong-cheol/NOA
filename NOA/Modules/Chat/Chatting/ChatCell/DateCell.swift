79252739ded6437e7a96e3cbbc674590f5d55e5f
wi-seong-cheol <tjd9248@naver.com>
2022-05-05 18:42:01 +0900

    DateCell Add

---
+++ NOA/Modules/Chat/Chatting/ChatCell/DateCell.swift
@@ -0,0 +1,47 @@
+//
+//  DateCell.swift
+//  NOA
+//
+//  Created by wi_seong on 2022/05/05.
+//
+
+import Foundation
+import RxSwift
+import UIKit
+
+class DateCell: UITableViewCell {
+    static let identifier = "DateCell"
+    
+    @IBOutlet weak var Date: UILabel!
+    
+    private let cellDisposeBag = DisposeBag()
+    
+    var disposeBag = DisposeBag()
+    let onData: AnyObserver<Lecture>
+    
+    required init?(coder aDecoder: NSCoder) {
+        let data = PublishSubject<Lecture>()
+        
+        onData = data.asObserver()
+        
+        super.init(coder: aDecoder)
+        
+        data.observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] lecture in
+            })
+            .disposed(by: cellDisposeBag)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+}
+
+
+
+
