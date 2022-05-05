547e6e545206755da7def5679ddffc4627244c09
wi-seong-cheol <tjd9248@naver.com>
2022-05-05 18:10:22 +0900

    Keyboard Setting

--- NOA/Modules/Chat/Chatting/ChattingViewController.swift
+++ NOA/Modules/Chat/Chatting/ChattingViewController.swift
@@ -10,11 +10,16 @@ import RxSwift
 import RxViewController
 import NVActivityIndicatorView
 import UIKit
+import SocketIO
+import RxKeyboard
 
 class ChattingViewController: UIViewController {
     
     static let identifier = "ChattingViewController"
-    @IBOutlet weak var work: UIImageView!
+    @IBOutlet weak var inputContainerBottomContraint: NSLayoutConstraint!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
     lazy var indicator: NVActivityIndicatorView = {
         let indicator = NVActivityIndicatorView(
@@ -51,10 +56,26 @@ class ChattingViewController: UIViewController {
         super.init(coder: aDecoder)
     }
     
+    // MARK: life cylce
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configure()
         setupBindings()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+//        self.tabBarController?.selectedIndex = 1
+        self.tabBarController?.tabBar.isHidden = false
     }
     
     override func didReceiveMemoryWarning() {
@@ -63,12 +84,41 @@ class ChattingViewController: UIViewController {
 }
 
 extension ChattingViewController {
-    // MARK: - UI Binding
-    func setupBindings() {
-        viewModel.work
-            .bind(to: work.rx.image)
+    // MARK: - UI Setting
+    func configure() {
+        let window = UIApplication.shared.windows.first
+        let extra = window!.safeAreaInsets.bottom
+        
+        // MARK: - Setting Keyboard
+        RxKeyboard.instance.visibleHeight
+            .skip(1)    // 초기 값 버리기
+            .drive(onNext: { keyboardVisibleHeight in
+                UIView.animate(withDuration: 0) {
+                    if keyboardVisibleHeight == 0 {
+                        self.inputContainerBottomContraint.constant = 0
+                    } else {
+                        self.inputContainerBottomContraint.constant = keyboardVisibleHeight - extra
+                    }
+                }
+                self.view.layoutIfNeeded()
+            })
             .disposed(by: disposeBag)
         
+        
+        let tapGuesterHideKeyboard = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tableView.addGestureRecognizer(tapGuesterHideKeyboard)
+    }
+    
+    // MARK: - UI Binding
+    func setupBindings() {
+        sendButton.rx.tap
+            .bind { [weak self] in
+                print("click")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    @objc func hideKeyboard() {
+        messageTextView.resignFirstResponder()
     }
 }
-
