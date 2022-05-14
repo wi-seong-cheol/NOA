e5bdb4750337559df6240f9efc1752fdbabec769
wi-seong-cheol <tjd9248@naver.com>
2022-05-15 00:01:30 +0900

    LoginViewModel Add

--- NOA/Modules/Login/LoginViewModel.swift
+++ NOA/Modules/Login/LoginViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+protocol LoginViewModelType {
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+    var activated: Observable<Bool> { get }
+    var errorMessage: Observable<NSError> { get }
+}
+
+class LoginViewModel: LoginViewModelType {
+    
+    let disposeBag = DisposeBag()
+    
+    // MARK: INPUT
+    
+    // MARK: OUTPUT
+    let activated: Observable<Bool>
+    let errorMessage: Observable<NSError>
+    
+    init(service: FeedFetchable = FeedService()) {
+        
+        let activating = BehaviorSubject<Bool>(value: false)
+        let error = PublishSubject<Error>()
+        
+        // MARK: INPUT
+                
+        // MARK: OUTPUT
+        
+        errorMessage = error.map { $0 as NSError }
+        
+        activated = activating.distinctUntilChanged()
+    }
+}
