aa0bc58b6b3aadf06d9af583b0aeee8fcfae4083
wi-seong-cheol <tjd9248@naver.com>
2022-05-20 00:02:00 +0900

    IsLogin Check

--- NOA/Modules/Splash/SplashViewModel.swift
+++ NOA/Modules/Splash/SplashViewModel.swift
@@ -16,6 +16,7 @@ protocol SplashViewModelType {
     // MARK: OUTPUT
     
     var lectureList: Observable<LectureList> { get }
+    var isLogin: Observable<Bool> { get }
     var activated: Observable<Bool> { get }
     var errorMessage: Observable<NSError> { get }
 }
@@ -31,31 +32,49 @@ class SplashViewModel: SplashViewModelType {
     // MARK: OUTPUT
     
     let lectureList: Observable<LectureList>
+    let isLogin: Observable<Bool>
     let activated: Observable<Bool>
     let errorMessage: Observable<NSError>
     
     init(service: UserFetchable = UserService()) {
         let fetching = PublishSubject<Void>()
+        let move = PublishSubject<Void>()
         
         let list = BehaviorSubject<LectureList>(value: LectureList.EMPTY)
         let activating = BehaviorSubject<Bool>(value: false)
+        let loginCheck = BehaviorSubject<Bool>(value: false)
         let error = PublishSubject<Error>()
         
         // MARK: INPUT
-        fetchList = fetching.asObserver()
+        fetchList = move.asObserver()
         
-        fetching
-            .do(onNext: { _ in activating.onNext(true) })
-            .flatMap(service.getTestData)
-            .do(onNext: { _ in activating.onNext(false) })
-            .do(onError: { err in error.onNext(err) })
-            .subscribe(onNext: list.onNext)
+        move
+            .subscribe(onNext: { _ in
+                if UserInfo.shared.getIsLogin() {
+                    loginCheck.onNext(true)
+                    print("true")
+                } else {
+                    loginCheck.onNext(false)
+                    print("false")
+                }
+            })
             .disposed(by: disposeBag)
+//            
+//        fetching
+//            .do(onNext: { _ in activating.onNext(true) })
+//            .flatMap(service.getTestData)
+//            .do(onNext: { _ in activating.onNext(false) })
+//            .do(onError: { err in error.onNext(err) })
+//            .subscribe(onNext: list.onNext)
+//            .disposed(by: disposeBag)
                 
+        
         // MARK: OUTPUT
         lectureList = list
 
         errorMessage = error.map { $0 as NSError }
+        
+        isLogin = loginCheck.map { $0 as Bool}
         
         activated = activating.distinctUntilChanged()
     }
