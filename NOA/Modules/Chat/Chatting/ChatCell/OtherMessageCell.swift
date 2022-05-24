51fea612b3dbe43892b9477ab515e6e980a0ed38
wi-seong-cheol <tjd9248@naver.com>
2022-05-24 16:11:30 +0900

    update UI

--- NOA/Modules/Chat/Chatting/ChatCell/OtherMessageCell.swift
+++ NOA/Modules/Chat/Chatting/ChatCell/OtherMessageCell.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 import UIKit
+
+protocol OtherMessageDelegate: AnyObject {
+    func didSelectedProfile(_ otherMessageCell: OtherMessageCell, detailButtonTappedFor userId: String)
+}
 
 class OtherMessageCell: UITableViewCell {
     static let identifier = "OtherMessageCell"
@@ -17,20 +22,25 @@ class OtherMessageCell: UITableViewCell {
     @IBOutlet weak var message: UILabel!
     @IBOutlet weak var timestamp: UILabel!
     
+    var delegate: OtherMessageDelegate?
+    
     private let cellDisposeBag = DisposeBag()
     
     var disposeBag = DisposeBag()
-    let onData: AnyObserver<Lecture>
+    let onData: AnyObserver<OtherMessageType>
+    let profileId = BehaviorRelay<String>(value: "")
     
     required init?(coder aDecoder: NSCoder) {
-        let data = PublishSubject<Lecture>()
+        let data = PublishSubject<OtherMessageType>()
         
         onData = data.asObserver()
         
         super.init(coder: aDecoder)
         
         data.observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] lecture in
+            .subscribe(onNext: { [weak self] msg in
+                self?.message.text = msg.message
+                self?.timestamp.text = msg.timestamp
             })
             .disposed(by: cellDisposeBag)
     }
@@ -39,11 +49,18 @@ class OtherMessageCell: UITableViewCell {
         super.awakeFromNib()
         
         profile.layer.cornerRadius = profile.frame.width / 2
+        
+        self.profile.isUserInteractionEnabled = true
+        self.profile.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.didSelectedProfile(_:))))
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         disposeBag = DisposeBag()
+    }
+    
+    @objc func didSelectedProfile(_ sender: UIButton) {
+        delegate?.didSelectedProfile(self, detailButtonTappedFor: profileId.value)
     }
 }
 
