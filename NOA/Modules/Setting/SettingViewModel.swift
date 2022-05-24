c3aa35afa661acd6bb5e68106b491bd9e2802feb
wi-seong-cheol <tjd9248@naver.com>
2022-05-24 16:15:24 +0900

    update

--- NOA/Modules/Setting/SettingViewModel.swift
+++ NOA/Modules/Setting/SettingViewModel.swift
@@ -12,8 +12,10 @@ import RealmSwift
 
 protocol SettingViewModelType {
     // MARK: INPUT
+    var logout: AnyObserver<Void> { get }
     
     // MARK: OUTPUT
+    var present: Observable<Bool> { get }
     var errorMessage: Observable<NSError> { get }
 }
 
@@ -23,16 +25,31 @@ class SettingViewModel: SettingViewModelType {
     
     let realm = try! Realm()
     // MARK: INPUT
+    let logout: AnyObserver<Void>
     
     // MARK: OUTPUT
+    let present: Observable<Bool>
     let errorMessage: Observable<NSError>
     
     init(service: FeedFetchable = FeedService()) {
+        let loggingOut = PublishSubject<Void>()
+        
         let error = PublishSubject<Error>()
+        let show = BehaviorSubject<Bool>(value: false)
         
         // MARK: INPUT
+        logout = loggingOut.asObserver()
+        
+        loggingOut
+            .subscribe(onNext: { _ in
+                
+                show.onNext(true)
+            })
+            .disposed(by: disposeBag)
         
         // MARK: OUTPUT
+        present = show.map { $0 as Bool }
+        
         errorMessage = error.map { $0 as NSError }
     }
 }
