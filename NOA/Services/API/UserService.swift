1d495e2d56c124847046d52e94d762511f782c77
wi-seong-cheol <tjd9248@naver.com>
2022-05-27 01:00:00 +0900

    Feat : "UserService Update"
    User API Service Function Add

--- NOA/Services/API/UserService.swift
+++ NOA/Services/API/UserService.swift
@@ -7,31 +7,106 @@
 
 import Foundation
 import RxSwift
+import Alamofire
 
 protocol UserFetchable {
-    func getUserData() -> Observable<[UserResponse]>
-    func getTestData()-> Observable<LectureList>
+//    func getSelfUser() -> Observable<UserResponse>
+    func getUser(_ otherId: String) -> Observable<UserResponse>
+    func follow(_ otherId: String) -> Observable<CommonResponse>
+    func isFriend(_ otherId: String) -> Observable<CommonResponse>
+    func friendList(_ page: Int) -> Observable<[UserResponse]>
+    func report(_ otherId: String) -> Observable<CommonResponse>
+    func profileEdit(_ data: Data) -> Observable<CommonResponse>
+    func nicknameEdit(_ nickname: String) -> Observable<CommonResponse>
+    func statusMessageEdit(_ message: String) -> Observable<CommonResponse>
 }
 
 class UserService: UserFetchable {
     
     @Inject private var apiUrlService: APIUrlService
     @Inject private var apiRequestService: APIRequestService
+//
+//    func getSelfUser() -> Observable<UserResponse> {
+//        let URL: String = apiUrlService.serviceUrl(.selfUser)
+//        let userId = UserInfo.shared.getUser().id
+//        let query: [String: Any] = ["user_code": userId]
+//
+//        return apiRequestService.getable(URL: URL, query: query, interceptor: .none) ?? Observable.empty()
+//    }
     
-    // User Data 조회 메소드
-    func getUserData()-> Observable<[UserResponse]> {
-//        let URL = apiUrlService.serviceUrl(version: "", path: "/courseList")
-        let URL = apiUrlService.serviceUrl(.duplication)
-        let query: [String: Any] = ["serviceKey": DBInfo.serviceKey, "Mobile": 1]
-        return apiRequestService.getable(URL: URL, query: query, interceptor: .none) ?? Observable.empty()
+    func getUser(_ otherId: String) -> Observable<UserResponse> {
+        let URL: String = apiUrlService.serviceUrl(.userInfo)
+        let userId = UserInfo.shared.getUser().id
+        let query: [String: Any] = ["user_code": userId,
+                                    "other": otherId]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.getable(URL: URL, query: query, interceptor: interceptor) ?? Observable.empty()
     }
     
-    // Test 메소드
-    func getTestData()-> Observable<LectureList> {
-        let URL = "http://apis.data.go.kr/B552881/kmooc/courseList?serviceKey=LwG%2BoHC0C5JRfLyvNtKkR94KYuT2QYNXOT5ONKk65iVxzMXLHF7SMWcuDqKMnT%2BfSMP61nqqh6Nj7cloXRQXLA%3D%3D"
-//        let URL = apiUrlService.serviceUrl(version: "", path: "/courseList")
-        let query: [String: Any] = ["Mobile": 1]
-//        let query: [String: Any] = ["serviceKey": DBInfo.serviceKey, "Mobile": 1]
-        return apiRequestService.getable(URL: URL, query: query, interceptor: .none) ?? Observable.empty()
+    func follow(_ otherId: String) -> Observable<CommonResponse> {
+        let URL: String = apiUrlService.serviceUrl(.friend)
+        let userId = UserInfo.shared.getUser().id
+        let body: [String: Any] = ["user_code": userId,
+                                    "friend_code": otherId]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.postable(URL: URL, body: body, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    func isFriend(_ otherId: String) -> Observable<CommonResponse> {
+        let URL: String = apiUrlService.serviceUrl(.isFriend)
+        let userId = UserInfo.shared.getUser().id
+        let query: [String: Any] = ["user_code": userId,
+                                    "friend_code": otherId]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.getable(URL: URL, query: query, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    func friendList(_ page: Int) -> Observable<[UserResponse]> {
+        let URL: String = apiUrlService.serviceUrl(.friendList)
+        let userId = UserInfo.shared.getUser().id
+        let query: [String: Any] = ["user_code": userId,
+                                    "page": page]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.getable(URL: URL, query: query, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    // 유저 신고
+    func report(_ otherId: String) -> Observable<CommonResponse> {
+        let URL = apiUrlService.serviceUrl(.reportUser)
+        let user = UserInfo.shared.getUser()
+        let userCode = user.id
+        let body: [String: Any] = ["user_code": userCode,
+                                   "reported_user": otherId]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.putable(URL: URL, body: body, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    func profileEdit(_ data: Data) -> Observable<CommonResponse> {
+        let URL = apiUrlService.serviceUrl(.changeProfile)
+        let user = UserInfo.shared.getUser()
+        let userCode = user.id
+        let body: [String: Any] = ["user_code": userCode]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.putUploadable(data: data, URL: URL, body: body, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    func nicknameEdit(_ nickname: String) -> Observable<CommonResponse> {
+        let URL = apiUrlService.serviceUrl(.changeNickname)
+        let user = UserInfo.shared.getUser()
+        let userCode = user.id
+        let body: [String: Any] = ["user_code": userCode,
+                                   "user_nickname": nickname]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.putable(URL: URL, body: body, interceptor: interceptor) ?? Observable.empty()
+    }
+    
+    func statusMessageEdit(_ message: String) -> Observable<CommonResponse> {
+        let URL = apiUrlService.serviceUrl(.changeDesc)
+        let user = UserInfo.shared.getUser()
+        let userCode = user.id
+        let body: [String: Any] = ["user_code": userCode,
+                                   "desc": message]
+        let interceptor: Interceptor = Interceptor(interceptors: [BaseInterceptor()])
+        return apiRequestService.putable(URL: URL, body: body, interceptor: interceptor) ?? Observable.empty()
     }
 }
