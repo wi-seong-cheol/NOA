d74bfa30a7ed0d4ea443bbf9c478aaf4eecc8f25
wi-seong-cheol <tjd9248@naver.com>
2022-05-30 01:30:00 +0900

    Feat : "Add isFirstTime Function"
    Check if this is your first visit

--- NOA/Utiles/Storage.swift
+++ NOA/Utiles/Storage.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public class Storage {
+    static func isFirstTime() -> Bool {
+        let defaults = UserDefaults.standard
+        if defaults.object(forKey: "isFirstTime") == nil {
+            defaults.set("No", forKey:"isFirstTime")
+            return true
+        } else {
+            return false
+        }
+    }
+}
