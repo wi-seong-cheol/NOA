32d1a82ecc9c9a0d8505e2dc0cc4b64966f033e6
wi-seong-cheol <tjd9248@naver.com>
2022-06-02 01:00:00 +0900

    Feat: "Add Auth Model"
    Auth Model add

--- NOA/Models/Auth/Auth.swift
+++ NOA/Models/Auth/Auth.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Duplicate: Codable {
+    let user_code: String
+    
+    enum CodingKeys: String, CodingKey {
+        case user_code
+    }
+
+    init(user_code: String) {
+        self.user_code = user_code
+    }
+}
+
+struct LoginResponse: NetworkResponse, Codable {
+    var status_code: Int
+    let data: LoginResult?
+    let message: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case status_code
+        case data
+        case message
+    }
+}
+
+struct LoginResult: Codable {
+    let user_code: Int
+    let accessToken: String
+    let refreshToken: String
+}
