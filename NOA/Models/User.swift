4dd8eb4b9d6a5b47dc848c7da8e2aed2d975b883
wi-seong-cheol <tjd9248@naver.com>
2022-06-03 01:30:00 +0900

    Feat: "Add User Model"
    User Model add

--- NOA/Models/User.swift
+++ NOA/Models/User.swift
@@ -9,36 +9,75 @@ import Foundation
 
 protocol UserType {
     var id: Int { get }
-    var nickname: Int { get }
+    var nickname: String { get }
     var profile: String { get }
-    var push_token: Int { get }
-}
-
-struct UserResponse: NetworkResponse, Codable {
-    let statusCode: Int
-    let body: User
+    var push_token: String { get }
 }
 
 struct User: UserType, Codable {
-    let id: Int
-    let nickname: Int
-    let profile: String
-    let push_token: Int
+    var id: Int
+    var nickname: String
+    var profile: String
+    var status_message: String
+    var push_token: String
     
     enum CodingKeys: String, CodingKey {
-        case id
+        case id = "user_code"
         case nickname
         case profile
+        case status_message
         case push_token
     }
 
     init(id: Int,
-         nickname: Int,
+         nickname: String,
          profile: String,
-         push_token: Int) {
+         status_message: String,
+         push_token: String) {
         self.id = id
         self.nickname = nickname
         self.profile = profile
+        self.status_message = status_message
         self.push_token = push_token
+    }
+}
+
+struct UserResponse: Codable {
+    let user_code: Int
+    let user_nickname: String
+    let user_profile: String
+    let user_wallet: String?
+    let user_report: Int?
+    let user_desc: String?
+    let isFriend: Int?
+    let isReport: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case user_code
+        case user_nickname
+        case user_profile
+        case user_wallet
+        case user_report
+        case user_desc
+        case isFriend = "isfriend"
+        case isReport
+    }
+
+    init(user_code: Int,
+         user_nickname: String,
+         user_profile: String,
+         user_wallet: String?,
+         user_report: Int?,
+         user_desc: String?,
+         isFriend: Int?,
+         isReport: Int?) {
+        self.user_code = user_code
+        self.user_nickname = user_nickname
+        self.user_profile = user_profile
+        self.user_wallet = user_wallet
+        self.user_report = user_report
+        self.user_desc = user_desc
+        self.isFriend = isFriend
+        self.isReport = isReport
     }
 }
