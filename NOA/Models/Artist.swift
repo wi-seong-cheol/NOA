401d5024d0d9e8b395238c61b4e65fa519bcedb3
wi-seong-cheol <tjd9248@naver.com>
2022-06-03 01:00:00 +0900

    Feat: "Add Artist Model"
    Artist Model add

--- NOA/Models/Artist.swift
+++ NOA/Models/Artist.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Artist: Codable {
+    let user_code: Int
+    let profile: String
+    let nickname: String
+    
+    enum CodingKeys: String, CodingKey {
+        case user_code
+        case profile
+        case nickname
+    }
+    
+    init(user_code: Int,
+         profile: String,
+         nickname: String) {
+        self.user_code = user_code
+        self.profile = profile
+        self.nickname = nickname
+    }
+}
+
+extension Artist {
+    static let EMPTY = Artist(user_code: 0, profile: "", nickname: "")
+}
