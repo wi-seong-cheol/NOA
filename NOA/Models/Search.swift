811672c7553ddb8e8700afd95610eb6b54cbed26
wi-seong-cheol <tjd9248@naver.com>
2022-06-04 01:00:00 +0900

    Feat: "Add Search Model"
    Search Model add

--- NOA/Models/Search.swift
+++ NOA/Models/Search.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Search: Codable {
+    
+    let post_id: Int
+    let post_title: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case post_id
+        case post_title
+    }
+
+    init(post_id: Int,
+         post_title: String?) {
+        self.post_id = post_id
+        self.post_title = post_title
+    }
+}
