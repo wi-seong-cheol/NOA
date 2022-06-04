c533126fcc6a44f07af7b1eefc52d3ac1823a2a6
wi-seong-cheol <tjd9248@naver.com>
2022-06-05 01:30:00 +0900

    Feat: "Add MyMessage Model"
    MyMessage Model add

--- NOA/Models/Message/MyMessage.swift
+++ NOA/Models/Message/MyMessage.swift
@@ -9,25 +9,40 @@ import Foundation
 
 protocol MyMessageType {
     var id: String { get }
+    var roomId: String { get }
+    var to: String { get }
+    var from: String { get }
     var message: String { get }
     var timestamp: String { get }
 }
 
 struct MyMessage: Codable, MyMessageType {
     let id: String
+    let roomId: String
+    let to: String
+    let from: String
     let message: String
     let timestamp: String
     
     enum CodingKeys: String, CodingKey {
         case id
+        case roomId
+        case to
+        case from
         case message
         case timestamp
     }
 
     init(id: String,
+         roomId: String,
+         to: String,
+         from: String,
          message: String,
          timestamp: String) {
         self.id = id
+        self.roomId = roomId
+        self.to = to
+        self.from = from
         self.message = message
         self.timestamp = timestamp
     }
