032dedeb63b379cfc9231d9dff6cd3fc521b89ef
wi-seong-cheol <tjd9248@naver.com>
2022-06-16 16:55:05 +0900

    Feat: "Add ChatRoom Model"
    ChatRoom Model Add

--- NOA/Models/Chat/ChatRoom.swift
+++ NOA/Models/Chat/ChatRoom.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol ChatRoomType {
+    var message: ChatRoomMessage { get }
+    var owner: Artist { get }
+    var roomId: Int { get }
+}
+
+struct ChatRoom: Codable, ChatRoomType {
+    let message: ChatRoomMessage
+    let owner: Artist
+    let roomId: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case message
+        case owner
+        case roomId = "room_id"
+    }
+    
+    init(message: ChatRoomMessage,
+         owner: Artist,
+         roomId: Int) {
+        self.message = message
+        self.owner = owner
+        self.roomId = roomId
+    }
+}
+
+extension ChatRoom {
+    static let EMPTY = ChatRoom(message: ChatRoomMessage.EMPTY, owner: Artist.EMPTY, roomId: 0)
+}
