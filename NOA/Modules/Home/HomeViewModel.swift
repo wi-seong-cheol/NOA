39e734cf5afa70459048b51d1e4c8aeb6edc9c18
wi-seong-cheol <tjd9248@naver.com>
2022-06-16 16:41:59 +0900

    Feat: "Add HomeViewModel Bind"
    HomeViewModel Bind

--- NOA/Modules/Home/HomeViewModel.swift
+++ NOA/Modules/Home/HomeViewModel.swift
@@ -8,86 +8,132 @@
 import Foundation
 import RxSwift
 import RxRelay
+import RxCocoa
 
 protocol HomeViewModelType {
+    associatedtype Input
+    associatedtype Output
+    
     // MARK: INPUT
-    var fetchList: AnyObserver<Void> { get }
-    var moreFetchList: AnyObserver<Void> { get }
-    var likeTap: AnyObserver<Void> { get }
+    var select$: BehaviorSubject<Int> { get }
+    var fetchList$: PublishSubject<Void> { get }
+    var moreFetchList$: PublishSubject<Void> { get }
+    var report$: PublishSubject<Int> { get }
     
     // MARK: OUTPUT
-    var items: Observable<[Lecture]> { get }
-    var activated: Observable<Bool> { get }
-    var errorMessage: Observable<NSError> { get }
+    var items$: BehaviorRelay<[Feed]> { get }
+    var activated$: Observable<Bool> { get }
+    var alertMessage$: Observable<String> { get }
+    var errorMessage$: Observable<Error> { get }
 }
 
 class HomeViewModel: HomeViewModelType {
     
     let disposeBag = DisposeBag()
     
+    struct Input {
+        var select: AnyObserver<Int>
+        var fetchList: AnyObserver<Void>
+        var moreFetchList: AnyObserver<Void>
+        var report: AnyObserver<Int>
+    }
+    
+    struct Output {
+        var items: Driver<[Feed]>
+        var activated: Observable<Bool>
+        var alertMessage: Observable<String>
+        var errorMessage: Observable<NSError>
+    }
+    
+    let input: Input
+    let output: Output
+    
     // MARK: INPUT
-    let fetchList: AnyObserver<Void>
-    let moreFetchList: AnyObserver<Void>
-    let likeTap: AnyObserver<Void>
+    internal let select$: BehaviorSubject<Int>
+    internal let fetchList$: PublishSubject<Void>
+    internal let moreFetchList$: PublishSubject<Void>
+    internal let report$: PublishSubject<Int>
     
     // MARK: OUTPUT
-    let items: Observable<[Lecture]>
-    let activated: Observable<Bool>
-    let errorMessage: Observable<NSError>
+    internal let items$: BehaviorRelay<[Feed]>
+    internal let activated$: Observable<Bool>
+    internal let alertMessage$: Observable<String>
+    internal let errorMessage$: Observable<Error>
     
     init(service: FeedFetchable = FeedService()) {
-        let fetching = PublishSubject<Void>()
-        let moreFetching = PublishSubject<Void>()
-        let liking = PublishSubject<Void>()
+        // MARK: INPUT
+        let select$ = BehaviorSubject<Int>(value: 0)
+        let fetchList$ = PublishSubject<Void>()
+        let moreFetchList$ = PublishSubject<Void>()
+        let report$ = PublishSubject<Int>()
         
-        let itemList = BehaviorRelay<[Lecture]>(value: [])
-        let nextPage = BehaviorRelay<String>(value: "")
-        let state = BehaviorRelay<Bool>(value: false)
-        
-        let activating = BehaviorSubject<Bool>(value: false)
-        let error = PublishSubject<Error>()
+        // MARK: OUTPUT
+        let items$ = BehaviorRelay<[Feed]>(value: [])
+        let page$ = BehaviorRelay<Int>(value: 0)
+//        let next$ = BehaviorRelay<String>(value: "")
+        let state$ = BehaviorRelay<Bool>(value: false)
+        let activated$ = BehaviorSubject<Bool>(value: false)
+        let alertMessage$ = BehaviorSubject<String>(value: "")
+        let errorMessage$ = PublishSubject<Error>()
         
         // MARK: INPUT
-        fetchList = fetching.asObserver()
-        moreFetchList = moreFetching.asObserver()
+        self.input = Input(select: select$.asObserver(),
+                           fetchList: fetchList$.asObserver(),
+                           moreFetchList: moreFetchList$.asObserver(),
+                           report: report$.asObserver())
         
-        fetching
-            .do(onNext: { _ in activating.onNext(true) })
-            .flatMap(service.list)
-            .do(onNext: { _ in activating.onNext(false) })
-            .do(onError: { err in error.onNext(err) })
+        self.select$ = select$
+        self.fetchList$ = fetchList$
+        self.moreFetchList$ = moreFetchList$
+        self.report$ = report$
+        
+        fetchList$
+            .do(onNext: { _ in activated$.onNext(true) })
+            .flatMapLatest{service.homeList(0)}
+            .do(onNext: { _ in activated$.onNext(false) })
+            .do(onError: { err in errorMessage$.onNext(err) })
             .subscribe(onNext: { response in
-                itemList.accept(response.lectures)
-                nextPage.accept(response.next)
+                items$.accept(response)
+                page$.accept(1)
             })
             .disposed(by: disposeBag)
                 
-        moreFetching
-            .filter{!state.value}
-            .do(onNext: { _ in state.accept(true)})
-            .do(onNext: { _ in activating.onNext(true) })
-            .flatMapLatest{ service.next(nextPage: nextPage.value) }
-            .do(onNext: { _ in activating.onNext(false) })
-            .do(onNext: { _ in state.accept(false)})
-            .do(onError: { err in error.onNext(err) })
+        moreFetchList$
+            .filter{ !state$.value }
+            .do(onNext: { _ in state$.accept(true)})
+            .do(onNext: { _ in activated$.onNext(true) })
+            .flatMapLatest{ service.homeList(page$.value) }
+            .do(onNext: { _ in activated$.onNext(false) })
+            .do(onError: { err in errorMessage$.onNext(err) })
             .subscribe(onNext: { response in
-                itemList.accept(itemList.value + response.lectures)
-                nextPage.accept(response.next)
+                items$.accept(items$.value + response)
+                page$.accept(page$.value + 1)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    state$.accept(false)
+                }
             })
             .disposed(by: disposeBag)
-                
-        likeTap = liking.asObserver()
-                
-//        liking.withLatestFrom(list)
-//                .map{ $0.lectures.map { $0.teachers("123") } }
-//            .subscribe(onNext: list.onNext)
-//                .disposed(by: disposeBag)
+        
+        report$
+            .map{ String($0) }
+            .do(onNext: { _ in activated$.onNext(true) })
+            .flatMapLatest{ service.report($0) }
+            .do(onNext: { _ in activated$.onNext(false) })
+            .do(onError: { err in errorMessage$.onNext(err) })
+            .subscribe(onNext: { response in
+                alertMessage$.onNext(response.message!)
+            })
+            .disposed(by: disposeBag)
                 
         // MARK: OUTPUT
-        items = itemList.asObservable()
-        
-        errorMessage = error.map { $0 as NSError }
-        
-        activated = activating.distinctUntilChanged()
+        self.output = Output(items: items$.asDriver(onErrorJustReturn: []),
+                             activated: activated$.distinctUntilChanged(),
+                             alertMessage: alertMessage$.map { $0 as String },
+                             errorMessage: errorMessage$.map { $0 as NSError })
+                
+        self.items$ = items$
+        self.activated$ = activated$
+        self.alertMessage$ = alertMessage$
+        self.errorMessage$ = errorMessage$
     }
 }
