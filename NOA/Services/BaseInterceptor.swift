dfc48bf46a7b742629f87bc0052ca2ba6877eb2d
wi-seong-cheol <tjd9248@naver.com>
2022-06-16 15:51:56 +0900

    Feat : "BaseInterceptor Update"
    Header에 Bearer Token 추가

--- NOA/Services/BaseInterceptor.swift
+++ NOA/Services/BaseInterceptor.swift
@@ -6,45 +6,47 @@
 //
 
 import Alamofire
-
 final class BaseInterceptor: RequestInterceptor {
-//    private let retryLimit = 1
-//    private var retryCount = 0
-//    
-//    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-//        print("BaseInterceptor  - adpat() called")
-//        
-//        var request = urlRequest
-//        let tk = Token()
-//        
-//        guard let accessToken = tk.load(account: "accessToken") else {
-//            return
-//        }
-//        
-//        request.headers.add(.authorization(bearerToken: accessToken))
-//        completion(.success(request))
-//    }
-//    
-//    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
-//        
-//        guard let statusCode = request.response?.statusCode else {
-//            completion(.doNotRetry)
-//            return
-//        }
-//        
-//        switch statusCode {
-//        case 401:
-//            if retryCount < retryLimit {
-//                retryCount += 1
-//                print("BaseInterceptor  - retry() called")
-//                let status = Methods.shared.refresh()
-//                print(status)
-//                completion(.retryWithDelay(1))
-//            } else {
-//                completion(.doNotRetry)
-//            }
-//        default:
-//            completion(.doNotRetry)
-//        }
-//    }
+    private let retryLimit = 1
+    private var retryCount = 0
+    
+    let service: AuthFetchable = AuthService()
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        print("BaseInterceptor  - adpat() called")
+        
+        var request = urlRequest
+        let tk = Token()
+        
+        guard let accessToken = tk.load(account: "accessToken") else {
+            return
+        }
+        
+        request.headers.add(.authorization(bearerToken: accessToken))
+        completion(.success(request))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        
+        guard let statusCode =
+                request.response?.statusCode else {
+            completion(.doNotRetry)
+            return
+        }
+        
+        switch statusCode {
+        case 401:
+            if retryCount < retryLimit {
+                retryCount += 1
+                print("BaseInterceptor  - retry() called")
+                let status = service.refresh()
+                print(status)
+                completion(.retryWithDelay(1))
+            } else {
+                completion(.doNotRetry)
+            }
+        default:
+            completion(.doNotRetry)
+        }
+    }
 }
