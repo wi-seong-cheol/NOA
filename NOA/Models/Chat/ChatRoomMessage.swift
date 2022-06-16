7ea198aef160841c1a728d028d20644bbeaef3ed
wi-seong-cheol <tjd9248@naver.com>
2022-06-16 16:55:41 +0900

    Feat: "Add ChatRoomMessage Model"
    ChatRoomMessage Model Add

--- NOA/Models/Chat/ChatRoomMessage.swift
+++ NOA/Models/Chat/ChatRoomMessage.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct ChatRoomMessage: Codable {
+    let message: String
+    let unreadCount: Int
+    let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case message
+        case unreadCount = "unread_message_count"
+        case created
+    }
+    
+    init(message: String,
+         unreadCount: Int,
+         created: String) {
+        self.message = message
+        self.unreadCount = unreadCount
+        self.created = created
+    }
+}
+
+extension ChatRoomMessage {
+    static let EMPTY = ChatRoomMessage(message: "", unreadCount: 0, created: "")
+}
