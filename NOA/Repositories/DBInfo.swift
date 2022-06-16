cf347f00297a246100fda413461eaa8c29af5854
wi-seong-cheol <tjd9248@naver.com>
2022-06-16 15:34:38 +0900

    DBInfo Update

--- NOA/Repositories/DBInfo.swift
+++ NOA/Repositories/DBInfo.swift
@@ -17,11 +17,13 @@ struct DBInfo{
     
     /*******************************************************/
     
-    static let service_url = "http://apis.data.go.kr/B552881/kmooc"
+//    static let service_url = "http://apis.data.go.kr/B552881/kmooc"
+    
+    static let service_url = "http://ec2-3-39-23-254.ap-northeast-2.compute.amazonaws.com:5000"
     
     /*******************************************************/
     // 채팅 서버
-    //static let chatting_url = "http://15.165.137.161:3030"
+    static let chatting_url = "http://ec2-3-39-23-254.ap-northeast-2.compute.amazonaws.com:5000"
     // MARK: exmpale
 //    static let chatting_url = "http://15.165.137.161:3030"
 //    static let chat_url = "https://smuls.com/"
@@ -33,11 +35,11 @@ struct DBInfo{
     
     /*******************************************************/
     // Infomation URL
-    static let dataPolicy: String =
-        "https://bronzed-prince-40e.notion.site/55b5d23bde8a49f0aa7993ba9591848d"
+    static let personalInformationPolicy: String =
+        "https://hhwooon.notion.site/e46cb2cfd1ad450d9149e728f0fe9a0b"
     
     static let termsOfService: String =
-        "https://bronzed-prince-40e.notion.site/55b5d23bde8a49f0aa7993ba9591848d"
+        "https://hhwooon.notion.site/f528f705e124402db96a891d4671efee"
     
     static let openSourceLicense: String =
         "https://bronzed-prince-40e.notion.site/55b5d23bde8a49f0aa7993ba9591848d"
@@ -47,5 +49,5 @@ struct DBInfo{
     
     static let contractAddress: String = "0x60ACe5b3B6b718dd75aE7450EFDd98dedFd13D87"
     
-    static let exhibitionUrl: String =  "https://noagallerybucket.s3.ap-northeast-2.amazonaws.com/test/index.html"
+    static let exhibitionUrl: String =  "http://noa-profile.s3.ap-northeast-2.amazonaws.com/unity/index.html"
 }
